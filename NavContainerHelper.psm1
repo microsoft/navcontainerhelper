53271c90817409baa6206f31eac123acba93a3e2
freddydk <freddy.kristiansen@microsoft.com>
2017-10-21 21:19:11 +0200

    Move welcome text to function

--- NavContainerHelper.psm1
+++ NavContainerHelper.psm1
@@ -13,63 +13,6 @@ $containerDemoFolder = "C:\DEMO"
 
 $sessions = @{}
 
-#region Print Welcome text
-
-clear
-Write-Host -ForegroundColor Yellow "Welcome to the Nav Container Helper PowerShell Prompt"
-Write-Host
-Write-Host -ForegroundColor Yellow "Container info functions"
-Write-Host "Get-NavContainerNavVersion    Get Nav version from Nav Container"
-Write-Host "Get-NavContainerImageName     Get ImageName from Nav container"
-Write-Host "Get-NavContainerGenericTag    Get Nav generic image tag from Nav container"
-Write-Host "Get-NavContainerOsVersion     Get OS version from Nav container"
-Write-Host "Get-NavContainerLegal         Get Legal link from Nav container"
-Write-Host "Get-NavContainerCountry       Get Localization version from Nav Container"
-Write-Host "Get-NavContainerIpAddress     Get IP Address to a Nav container"
-Write-Host "Get-NavContainerSharedFolders Get Shared Folders from a Nav container"
-Write-Host "Get-NavContainerPath          Get the path inside a Nav container to a shared file"
-Write-Host "Get-NavContainerName          Get the name of a Nav container"
-Write-Host "Get-NavContainerId            Get the Id of a Nav container"
-Write-Host "Test-NavContainer             Test whether a Nav container exists"
-Write-Host
-Write-Host -ForegroundColor Yellow "Container handling functions"
-Write-Host "New-CSideDevContainer         Create new C/SIDE development container"
-Write-Host "Remove-CSideDevContainer      Remove C/SIDE development container"
-Write-Host "Get-NavContainerSession       Create new session to a Nav container"
-Write-Host "Remove-NavContainerSession    Remove Nav container session"
-Write-Host "Enter-NavContainer            Enter Nav container session"
-Write-Host "Open-NavContainer             Open Nav container in new window"
-Write-Host "Wait-NavContainerReady        Wait for Nav Container to become ready"
-Write-Host
-Write-Host -ForegroundColor Yellow "Object handling functions"
-Write-Host "Import-ObjectsToNavContainer  Import objects from .txt or .fob file"
-Write-Host "Compile-ObjectsInNavContainer Compile objects"
-Write-Host "Export-NavContainerObjects    Export objects from Nav container"
-Write-Host "Create-MyOriginalFolder       Create folder with the original objects for modified objects"
-Write-Host "Create-MyDeltaFolder          Create folder with deltas for modified objects"
-Write-Host "Convert-Txt2Al                Convert deltas folder to al folder"
-Write-Host "Convert-ModifiedObjectsToAl   Export objects, create baseline, create deltas and convert to .al files"
-Write-Host
-Write-Host -ForegroundColor Yellow "App handling functions"
-Write-Host "Publish-NavContainerApp       Publish App to Nav container"
-Write-Host "Sync-NavContainerApp          Sync App in Nav container"
-Write-Host "Install-NavContainerApp       Install App in Nav container"
-Write-Host "Uninstall-NavContainerApp     Uninstall App from Nav container"
-Write-Host "Unpublish-NavContainerApp     Unpublish App from Nav container"
-Write-Host "Get-NavContainerAppInfo       Get info about installed apps from Nav Container"
-Write-Host "Install-NAVSipCryptoProviderFromNavContainer Install Nav Sip Crypto Provider locally from container to sign extensions"
-Write-Host
-Write-Host -ForegroundColor Yellow "Azure VM specific functions"
-Write-Host "Replace-NavServerContainer    Replace navserver (primary) container"
-Write-Host "Recreate-NavServerContainer   Recreate navserver (primary) container"
-Write-Host
-Write-Host -ForegroundColor White "Note: The Nav Container Helper is an open source project from http://www.github.com/microsoft/navcontainerhelper."
-Write-Host -ForegroundColor White "The project is released as-is, no warranty! Contributions are welcome, study the github repository for usage."
-Write-Host -ForegroundColor White "Report issues on http://www.github.com/microsoft/navcontainerhelper/issues."
-Write-Host
-
-#endregion Print Welcome text
-
 #region Internal helper functions
 
 function Log([string]$line, [string]$color = "Gray") { 
@@ -1789,3 +1732,61 @@ function Remove-DesktopShortcut {
     }
 }
 Export-ModuleMember Remove-DesktopShortcut
+
+#region Print Welcome text
+function Write-NavContainerHelperWelcomeText {
+    clear
+    Write-Host -ForegroundColor Yellow "Welcome to the Nav Container Helper PowerShell Prompt"
+    Write-Host
+    Write-Host -ForegroundColor Yellow "Container info functions"
+    Write-Host "Get-NavContainerNavVersion    Get Nav version from Nav Container"
+    Write-Host "Get-NavContainerImageName     Get ImageName from Nav container"
+    Write-Host "Get-NavContainerGenericTag    Get Nav generic image tag from Nav container"
+    Write-Host "Get-NavContainerOsVersion     Get OS version from Nav container"
+    Write-Host "Get-NavContainerLegal         Get Legal link from Nav container"
+    Write-Host "Get-NavContainerCountry       Get Localization version from Nav Container"
+    Write-Host "Get-NavContainerIpAddress     Get IP Address to a Nav container"
+    Write-Host "Get-NavContainerSharedFolders Get Shared Folders from a Nav container"
+    Write-Host "Get-NavContainerPath          Get the path inside a Nav container to a shared file"
+    Write-Host "Get-NavContainerName          Get the name of a Nav container"
+    Write-Host "Get-NavContainerId            Get the Id of a Nav container"
+    Write-Host "Test-NavContainer             Test whether a Nav container exists"
+    Write-Host
+    Write-Host -ForegroundColor Yellow "Container handling functions"
+    Write-Host "New-CSideDevContainer         Create new C/SIDE development container"
+    Write-Host "Remove-CSideDevContainer      Remove C/SIDE development container"
+    Write-Host "Get-NavContainerSession       Create new session to a Nav container"
+    Write-Host "Remove-NavContainerSession    Remove Nav container session"
+    Write-Host "Enter-NavContainer            Enter Nav container session"
+    Write-Host "Open-NavContainer             Open Nav container in new window"
+    Write-Host "Wait-NavContainerReady        Wait for Nav Container to become ready"
+    Write-Host
+    Write-Host -ForegroundColor Yellow "Object handling functions"
+    Write-Host "Import-ObjectsToNavContainer  Import objects from .txt or .fob file"
+    Write-Host "Compile-ObjectsInNavContainer Compile objects"
+    Write-Host "Export-NavContainerObjects    Export objects from Nav container"
+    Write-Host "Create-MyOriginalFolder       Create folder with the original objects for modified objects"
+    Write-Host "Create-MyDeltaFolder          Create folder with deltas for modified objects"
+    Write-Host "Convert-Txt2Al                Convert deltas folder to al folder"
+    Write-Host "Convert-ModifiedObjectsToAl   Export objects, create baseline, create deltas and convert to .al files"
+    Write-Host
+    Write-Host -ForegroundColor Yellow "App handling functions"
+    Write-Host "Publish-NavContainerApp       Publish App to Nav container"
+    Write-Host "Sync-NavContainerApp          Sync App in Nav container"
+    Write-Host "Install-NavContainerApp       Install App in Nav container"
+    Write-Host "Uninstall-NavContainerApp     Uninstall App from Nav container"
+    Write-Host "Unpublish-NavContainerApp     Unpublish App from Nav container"
+    Write-Host "Get-NavContainerAppInfo       Get info about installed apps from Nav Container"
+    Write-Host "Install-NAVSipCryptoProviderFromNavContainer Install Nav Sip Crypto Provider locally from container to sign extensions"
+    Write-Host
+    Write-Host -ForegroundColor Yellow "Azure VM specific functions"
+    Write-Host "Replace-NavServerContainer    Replace navserver (primary) container"
+    Write-Host "Recreate-NavServerContainer   Recreate navserver (primary) container"
+    Write-Host
+    Write-Host -ForegroundColor White "Note: The Nav Container Helper is an open source project from http://www.github.com/microsoft/navcontainerhelper."
+    Write-Host -ForegroundColor White "The project is released as-is, no warranty! Contributions are welcome, study the github repository for usage."
+    Write-Host -ForegroundColor White "Report issues on http://www.github.com/microsoft/navcontainerhelper/issues."
+    Write-Host
+}
+Export-ModuleMember Write-NavContainerHelperWelcomeText
+#endregion Print Welcome text
