a59b9a20cb046145c3c4b1959253d1d1092f0cb5
freddydk <freddy.kristiansen@microsoft.com>
2017-12-11 10:35:41 +0100

    Get-NavContainers

---
+++ ContainerInfo/Get-NavContainers.ps1
@@ -0,0 +1,24 @@
+﻿<# 
+ .Synopsis
+  Get a list of all NAV Containers
+ .Description
+  Returns the names of all NAV Containers
+ .Example
+  Get-NavContainers | Remove-NavContainer
+#>
+function Get-NavContainers {
+    Process {
+        docker ps -a -q --no-trunc | % {
+            $inspect = docker inspect $_ | ConvertFrom-Json
+            if ($inspect.Config.Labels.psobject.Properties.Match('nav').Count -ne 0) {
+                $name = $inspect.Name
+                if ($name.startsWith('/')) {
+                    $name.subString(1)
+                } else {
+                    $name
+                }
+            }
+        }
+    }
+}
+Export-ModuleMember -function Get-NavContainers
