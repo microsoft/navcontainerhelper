d38832712d66ecc12f9761fbbb5de948e4664a2e
freddydk <freddy.kristiansen@microsoft.com>
2018-03-29 09:00:21 +0200

    Added TLS1.2 support to Download-File

--- Misc/Download-File.ps1
+++ Misc/Download-File.ps1
@@ -8,6 +8,7 @@
 
     Write-Host "Downloading $destinationFile"
     Remove-Item -Path $destinationFile -Force -ErrorAction Ignore
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
     (New-Object System.Net.WebClient).DownloadFile($sourceUrl, $destinationFile)
 }
 Export-ModuleMember Download-File
