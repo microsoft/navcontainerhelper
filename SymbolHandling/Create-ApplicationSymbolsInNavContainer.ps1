54b3b47a028ebf0e4d63f5b67ccf891135e076fb
Mike Glue <mglue@axentia.ca>
2018-08-07 15:36:51 -0400

    remove old code

--- SymbolHandling/Create-ApplicationSymbolsInNavContainer.ps1
+++ SymbolHandling/Create-ApplicationSymbolsInNavContainer.ps1
@@ -46,51 +46,5 @@ function Create-ApplicationSymbolsInNavContainer {
     } -ArgumentList $containerName
     
     Write-Host -ForegroundColor Green "Application symbols successfully created"
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Invoke-Command -Session $session -ScriptBlock { Param($filter, [System.Management.Automation.PSCredential]$sqlCredential)
-
-        $customConfigFile = Join-Path (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName "CustomSettings.config"
-        [xml]$customConfig = [System.IO.File]::ReadAllText($customConfigFile)
-        $databaseServer = $customConfig.SelectSingleNode("//appSettings/add[@key='DatabaseServer']").Value
-        $databaseInstance = $customConfig.SelectSingleNode("//appSettings/add[@key='DatabaseInstance']").Value
-        $databaseName = $customConfig.SelectSingleNode("//appSettings/add[@key='DatabaseName']").Value
-        if ($databaseInstance) { $databaseServer += "\$databaseInstance" }
-
-        $enableSymbolLoadingKey = $customConfig.SelectSingleNode("//appSettings/add[@key='EnableSymbolLoadingAtServerStartup']")
-        if ($enableSymbolLoadingKey -ne $null -and $enableSymbolLoadingKey.Value -eq "True") {
-            # HACK: Parameter insertion...
-            # generatesymbolreference is not supported by Compile-NAVApplicationObject yet
-            # insert an extra parameter for the finsql command by splitting the filter property
-            $filter = '",generatesymbolreference=1,filter="' + $filter
-        }
-
-        $params = @{}
-        if ($sqlCredential) {
-            $params = @{ 'Username' = $sqlCredential.UserName; 'Password' = ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($sqlCredential.Password))) }
-        }
-        Write-Host "Compiling objects"
-        Compile-NAVApplicationObject @params -Filter $filter `
-            -DatabaseName $databaseName `
-            -DatabaseServer $databaseServer `
-            -Recompile `
-            -SynchronizeSchemaChanges Force `
-            -NavServerName localhost `
-            -NavServerInstance NAV `
-            -NavServerManagementPort 7045
-
-    } -ArgumentList $filter, $sqlCredential
-    Write-Host -ForegroundColor Green "Objects successfully compiled"
 }
 Export-ModuleMember -Function Create-ApplicationSymbolsInNavContainer
