ad29b9b56d5e3c6d53881d85463ec9d3c8a609b9
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-08-26 12:08:45 +0200

    Add publish script

--- NavContainerHelper.ps1
+++ NavContainerHelper.ps1
@@ -1,3 +1,5 @@
 ﻿Remove-Module NavContainerHelper -ErrorAction Ignore
+Uninstall-module NavContainerHelper -ErrorAction Ignore
+
 $modulePath = Join-Path $PSScriptRoot "NavContainerHelper.psm1"
 Import-Module $modulePath -DisableNameChecking
---
+++ PublishNavContainerHelper.ps1
@@ -0,0 +1,19 @@
+﻿$VerbosePreference="SilentlyContinue"
+
+# Version, Author, CompanyName and nugetkey
+. (Join-Path $PSScriptRoot "settings.ps1")
+
+Clear-Host
+Invoke-ScriptAnalyzer -Path $PSScriptRoot -Recurse -Settings PSGallery -Severity Warning
+
+. (Join-Path $PSScriptRoot "NavContainerHelper.ps1")
+$functionsToExport = (get-module -Name NavContainerHelper).ExportedFunctions.Keys | Sort-Object
+Update-ModuleManifest -Path (Join-Path $PSScriptRoot "NavContainerHelper.psd1") `
+                      -RootModule "NavContainerHelper.psm1" `
+                      -ModuleVersion $version `
+                      -Author $author `
+                      -FunctionsToExport $functionsToExport `
+                      -CompanyName $CompanyName `
+                      -ReleaseNotes (get-content (Join-Path $PSScriptRoot "ReleaseNotes.txt")) 
+
+Publish-Module -Path $PSScriptRoot -NuGetApiKey $nugetkey
