230433d45f0a410983c63e5478ee2e59394a53c1
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-01-26 09:36:01 +0100

    Fix or #313

--- ObjectHandling/Import-TestToolkitToNavContainer.ps1
+++ ObjectHandling/Import-TestToolkitToNavContainer.ps1
@@ -14,6 +14,8 @@
   Only import TestToolkit objects for a specific country.
   You must specify the country code that is used in the TestToolkit object name (e.g. CA, US, MX, etc.).
   This parameter only needs to be used in the event there are multiple country-specific sets of objects in the TestToolkit folder.
+ .Parameter doNotUpdateSymbols
+  Add this switch to avoid updating symbols when importing the test toolkit
  .Example
   Import-TestToolkitToNavContainer -containerName test2
   .Example
@@ -25,12 +27,13 @@ function Import-TestToolkitToNavContainer {
         [string]$containerName, 
         [System.Management.Automation.PSCredential]$sqlCredential = $null,
         [switch]$includeTestLibrariesOnly,
-        [string]$testToolkitCountry
+        [string]$testToolkitCountry,
+        [switch]$doNotUpdateSymbols
     )
 
     $sqlCredential = Get-DefaultSqlCredential -containerName $containerName -sqlCredential $sqlCredential
 
-    Invoke-ScriptInNavContainer -containerName $containerName -ScriptBlock { Param([System.Management.Automation.PSCredential]$sqlCredential, $includeTestLibrariesOnly, $testToolkitCountry)
+    Invoke-ScriptInNavContainer -containerName $containerName -ScriptBlock { Param([System.Management.Automation.PSCredential]$sqlCredential, $includeTestLibrariesOnly, $testToolkitCountry, $doNotUpdateSymbols)
     
         $customConfigFile = Join-Path (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName "CustomSettings.config"
         [xml]$customConfig = [System.IO.File]::ReadAllText($customConfigFile)
@@ -56,7 +59,8 @@ function Import-TestToolkitToNavContainer {
                 $objectsFile = $_.FullName
                 Write-Host "Importing Objects from $objectsFile (container path)"
                 $databaseServerParameter = $databaseServer
-                if ($enableSymbolLoadingKey -ne $null -and $enableSymbolLoadingKey.Value -eq "True") {
+
+                if ($enableSymbolLoadingKey -ne $null -and $enableSymbolLoadingKey.Value -eq "True" -and !$doNotUpdateSymbols) {
                     # HACK: Parameter insertion...
                     # generatesymbolreference is not supported by Import-NAVApplicationObject yet
                     # insert an extra parameter for the finsql command by splitting the filter property
@@ -79,7 +83,7 @@ function Import-TestToolkitToNavContainer {
         # Sync after all objects hav been imported
          Get-NAVTenant NAV | Sync-NavTenant -Mode ForceSync -Force
 
-    } -ArgumentList $sqlCredential, $includeTestLibrariesOnly, $testToolkitCountry
+    } -ArgumentList $sqlCredential, $includeTestLibrariesOnly, $testToolkitCountry, $doNotUpdateSymbols
     Write-Host -ForegroundColor Green "TestToolkit successfully imported"
 }
 Export-ModuleMember -Function Import-TestToolkitToNavContainer
