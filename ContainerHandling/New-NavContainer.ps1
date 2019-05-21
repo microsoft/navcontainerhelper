a0bdbc712a319f8305fdef9446141c5d18c1f18d
Tobias Fenster <tobias.fenster@axians-infoma.de>
2019-05-21 11:15:21 +0200

    only use PublicDnsName from file if not given as param

--- ContainerHandling/New-NavContainer.ps1
+++ ContainerHandling/New-NavContainer.ps1
@@ -269,7 +269,7 @@ function New-NavContainer {
         $useSSL = $false
     }
 
-    if (Test-Path "C:\inetpub\wwwroot\hostname.txt") {
+    if (Test-Path "C:\inetpub\wwwroot\hostname.txt" -and -not $PublicDnsName) {
         $PublicDnsName = Get-Content -Path "C:\inetpub\wwwroot\hostname.txt" 
     }
     if (-not $PublicDnsName -and $useTraefik) {
