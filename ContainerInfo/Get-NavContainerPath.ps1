f20c7a0e1440408c35219e467069ecd2cebbda10
Freddy Kristiansen <freddyk@microsoft.com>
2019-11-09 13:13:04 +0100

    Failing test

--- ContainerInfo/Get-NavContainerPath.ps1
+++ ContainerInfo/Get-NavContainerPath.ps1
@@ -38,8 +38,8 @@ function Get-NavContainerPath {
         } else {
             $sharedFolders = Get-NavContainerSharedFolders -containerName $containerName
             $sharedFolders.GetEnumerator() | ForEach-Object {
-                if ($path -eq $_.Name -or ($containerPath -eq "" -and $path.StartsWith((Join-Path $_.Name "\"), "OrdinalIgnoreCase"))) {
-                    $containerPath = ((Join-Path $_.Value $path.Substring($_.Name.Length)))
+                if ($path -eq $_.Name -or ($containerPath -eq "" -and $path.StartsWith($_.Name.TrimEnd('\')+'\', "OrdinalIgnoreCase"))) {
+                    $containerPath = ($_.Value.TrimEnd('\') + '\' + $path.Substring($_.Name.Length).TrimStart('\'))
                 }
             }
             if ($throw -and "$containerPath" -eq "") {
