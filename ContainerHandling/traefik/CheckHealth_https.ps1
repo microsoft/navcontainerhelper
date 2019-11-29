6401d1792b9d091250daa163e00640cb96b3edc3
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2019-11-29 10:38:25 +0100

    handle self-signed certs during health check

--- ContainerHandling/traefik/CheckHealth_https.ps1
+++ ContainerHandling/traefik/CheckHealth_https.ps1
@@ -1,3 +1,23 @@
+if (-not("dummy" -as [type])) {
+    add-type -TypeDefinition @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class Dummy {
+    public static bool ReturnTrue(object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+    public static RemoteCertificateValidationCallback GetDelegate() {
+        return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
+    }
+}
+"@
+}
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
+
 $healthcheckurl = ("https://localhost/" + $env:webserverinstance + "/")
 try {
     $result = Invoke-WebRequest -Uri "${healthcheckurl}Health/System" -UseBasicParsing -TimeoutSec 10
