ab9b3a6cd8fccae3cbf222499adba945aa017d47
Erik P. Ernst <epernst@dynamics-experts.net>
2019-12-02 11:47:09 +0100

    Added parameter documentation

--- TenantHandling/Get-NavContainerTenants.ps1
+++ TenantHandling/Get-NavContainerTenants.ps1
@@ -5,6 +5,12 @@
   Get information about all tenants in the Container
  .Parameter containerName
   Name of the container from which you want to get the tenant information
+ .Parameter Tenant
+  Specifies the ID of the specific tenant that you want to get information about, such as Tenant1.
+ .Parameter forceRefresh
+  Specifies to update a tenant's state and data version based, in part, on the data version of the tenant database that contains the tenant. 
+ .Parameter force
+  Forces the command to run without asking for user confirmation.
  .Example
   Get-NavContainerTenants -containerName test
 #>
@@ -27,7 +33,7 @@ function Get-NavContainerTenants {
     If ($ForceRefresh) {
         $Params += @{
             "ForceRefresh" = $ForceRefresh
-            "Tenant" = $Tenant
+            "Tenant"       = $Tenant
         }
     }
     Invoke-ScriptInNavContainer -containerName $containerName -ScriptBlock {
