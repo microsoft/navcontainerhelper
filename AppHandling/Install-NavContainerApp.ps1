8cc6f29ff3f09787d116f21c9ce29398024a63ae
Erik P. Ernst <epernst@dynamics-experts.net>
2020-01-04 15:24:38 +0100

    Added support for Force

--- AppHandling/Install-NavContainerApp.ps1
+++ AppHandling/Install-NavContainerApp.ps1
@@ -11,33 +11,36 @@
   Name of app you want to install in the container
  .Parameter appVersion
   Versin of app you want to install in the container
+ .Parameter force
+  Include this flag to indicate that you want to force install the app
  .Example
   Install-NavContainerApp -containerName test2 -appName myapp
 #>
 function Install-NavContainerApp {
     Param (
         [string] $containerName = "navserver",
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [string] $tenant = "default",
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string] $appName,
         [Parameter()]
-        [string] $appVersion
+        [string] $appVersion,
+        [switch] $Force
     )
 
-    Invoke-ScriptInNavContainer -containerName $containerName -ScriptBlock { Param($appName, $appVersion, $tenant)
+    Invoke-ScriptInNavContainer -containerName $containerName -ScriptBlock { Param($appName, $appVersion, $tenant, $Force)
         Write-Host "Installing $appName on $tenant"
         $parameters = @{ 
             "ServerInstance" = $ServerInstance
-            "Name" = $appName
-            "Tenant" = $tenant
+            "Name"           = $appName
+            "Tenant"         = $tenant
+            "Force"          = $Force
         }
-        if ($appVersion)
-        {
+        if ($appVersion) {
             $parameters += @{ "Version" = $appVersion }
         }
         Install-NavApp @parameters
-    } -ArgumentList $appName, $appVersion, $tenant
+    } -ArgumentList $appName, $appVersion, $tenant, $Force
     Write-Host -ForegroundColor Green "App successfully installed"
 }
 Set-Alias -Name Install-BCContainerApp -Value Install-NavContainerApp
