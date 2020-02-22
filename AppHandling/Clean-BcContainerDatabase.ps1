986cb4dcf15e0b9fc8f18e22210bac2670b2377b
Freddy Kristiansen <freddyk@microsoft.com>
2020-02-22 12:44:44 +0100

    Issue #863

--- AppHandling/Clean-BcContainerDatabase.ps1
+++ AppHandling/Clean-BcContainerDatabase.ps1
@@ -172,7 +172,12 @@ function Clean-BcContainerDatabase {
             Invoke-ScriptInBCContainer -containerName $containerName -scriptblock { Param($app, $SaveData, $onlySaveBaseAppData)
                 if ($app.IsInstalled) {
                     Write-Host "Uninstalling $($app.Name)"
-                    $app | Uninstall-NavApp -Force -doNotSaveData:(!$SaveData -or ($Name -ne "BaseApp" -and $Name -ne "Base Application" -and $onlySaveBaseAppData))
+                    $tenant = "Default"
+                    if ($app.Tenant)
+                    {
+                      $tenant = $app.Tenant
+                    }
+                    $app | Uninstall-NavApp -tenant $tenant -Force -doNotSaveData:(!$SaveData -or ($Name -ne "BaseApp" -and $Name -ne "Base Application" -and $onlySaveBaseAppData))
                 }
             } -argumentList $app, $SaveData, $onlySaveBaseAppData
         }
