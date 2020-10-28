a3d84818f856942d5f64c2d8f369b0d10da3dbca
Freddy Kristiansen <freddyk@microsoft.com>
2020-10-28 12:21:05 +0100

    version number + descriptions

--- AppHandling/Publish-BuildOutputToStorage.ps1
+++ AppHandling/Publish-BuildOutputToStorage.ps1
@@ -1,23 +1,45 @@
 ﻿<# 
  .Synopsis
-  Preview function for publishing build output to storage account
+  Function for publishing build output from Run-AlPipeline to storage account
  .Description
-  Preview function for publishing build output to storage account
+  Function for publishing build output to storage account
+  The function will publish artifacts in the format of https://businesscentralapps.blob.core.windows.net/bingmaps/16.0.10208.0/apps.zip
+  Please consult the CI/CD Workshop document at http://aka.ms/cicdhol to learn more about this function
+ .Parameter StorageConnectionString
+  A connectionstring with access to the storage account in which you want to publish artifacts (SecureString or String)
+ .Parameter projectName
+  Project name of the app you want to publish. This becomes part of the blob url.
+ .Parameter appVersion
+  Version of the app you want to publish. This becomes part of the blob url.
+ .Parameter permission
+  Specifies the public level access to the container (if it gets created by this function)
+  Default is Container, which provides full access. Other values are Blob or Off.
+ .Parameter path
+  Path containing the build output from Run-AlPipeline.
+  The content of folders Apps, RuntimePackages and TestApps from this folder is published.
+ .Parameter setLatest
+  Add this switch if you want this artifact to also be published as latest
 #>
 function Publish-BuildOutputToStorage {
     Param(
         [Parameter(Mandatory=$true)]
-        [string] $StorageConnectionString,
+        $StorageConnectionString,
         [Parameter(Mandatory=$true)]
         [string] $projectName,
         [Parameter(Mandatory=$true)]
         [string] $appVersion,
         [Parameter(Mandatory=$false)]
+        [ValidateSet('Container','Blob','Off')]
         [string] $permission = "Container",
         [Parameter(Mandatory=$true)]
         [string] $path,
         [switch] $setLatest
     )
+
+    if ($StorageConnectionString -is [SecureString]) { $StorageConnectionString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($StorageConnectionString)) }
+    if ($StorageConnectionString -isnot [string]) { throw "StorageConnectionString needs to be a SecureString or a String" }
+    $projectName = $projectName.ToLowerInvariant()
+    $appVersion = $appVersion.ToLowerInvariant()
 
     if (!(get-command New-AzureStorageContext -ErrorAction SilentlyContinue)) {
         Set-Alias -Name New-AzureStorageContext -Value New-AzStorageContext
