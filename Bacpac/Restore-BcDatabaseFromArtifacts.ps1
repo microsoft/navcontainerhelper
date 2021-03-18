79cbbed74bf3ff794905956ac540099a9765d189
KSACEK <kamil.sacek@navertica.com>
2021-03-18 08:02:50 +0100

    Using SqlServer PS module to load the Smo libraries
    Solve #1800

--- Bacpac/Restore-BcDatabaseFromArtifacts.ps1
+++ Bacpac/Restore-BcDatabaseFromArtifacts.ps1
@@ -79,8 +79,6 @@ function Restore-BcDatabaseFromArtifacts {
         if ($databaseInstance) {
             $databaseServerInstance += "\$databaseInstance"
         }
-        Import-Module sqlps
-        $smoServer = New-Object Microsoft.SqlServer.Management.Smo.Server $databaseServerInstance
 
         $dbName = "$databasePrefix$databaseName"
         if ($multitenant) {
@@ -90,11 +88,14 @@ function Restore-BcDatabaseFromArtifacts {
         New-NAVDatabase -DatabaseServer $databaseServer -DatabaseInstance $databaseInstance -DatabaseName $dbName -FilePath $bakFile -DestinationPath (Join-Path $smoServer.RootDirectory "DATA\$($databaseprefix -replace '[^a-zA-Z0-9]', '')") | Out-Null
    
         if ($multitenant) {
-            $Smo = [reflection.assembly]::Load("Microsoft.SqlServer.Smo, Version=$($smoServer.VersionMajor).$($smoServer.VersionMinor).0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-            $SmoExtended = [reflection.assembly]::Load("Microsoft.SqlServer.SmoExtended, Version=$($smoServer.VersionMajor).$($smoServer.VersionMinor).0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-            $ConnectionInfo = [reflection.assembly]::Load("Microsoft.SqlServer.ConnectionInfo, Version=$($smoServer.VersionMajor).$($smoServer.VersionMinor).0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-            $SqlEnum = [reflection.assembly]::Load("Microsoft.SqlServer.SqlEnum, Version=$($smoServer.VersionMajor).$($smoServer.VersionMinor).0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-            
+            import-module SqlServer
+            $SqlModule = get-module SqlServer
+            $Path = Split-Path $SqlModule.Path
+            $Smo = [Reflection.Assembly]::LoadFile((Join-Path $Path 'Microsoft.SqlServer.Smo.dll'))
+            $SmoExtended = [Reflection.Assembly]::LoadFile((Join-Path $Path 'Microsoft.SqlServer.SmoExtended.dll'))
+            $ConnectionInfo = [Reflection.Assembly]::LoadFile((Join-Path $Path 'Microsoft.SqlServer.ConnectionInfo.dll'))
+            $SqlEnum = [Reflection.Assembly]::LoadFile((Join-Path $Path 'Microsoft.SqlServer.SqlEnum.dll'))
+          
             $OnAssemblyResolve = [System.ResolveEventHandler] {
                 param($sender, $e)
                 if ($e.Name -like "Microsoft.SqlServer.Smo, Version=*, Culture=neutral, PublicKeyToken=89845dcd8080cc91") { return $Smo }
