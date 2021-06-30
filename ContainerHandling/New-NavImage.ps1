1f62c9ce09ca53b306b3cabce62dda734507d3d6
Freddy Kristiansen <freddyk@microsoft.com>
2021-06-30 13:04:10 +0200

    Use process isolation when building image

--- ContainerHandling/New-NavImage.ps1
+++ ContainerHandling/New-NavImage.ps1
@@ -310,6 +310,12 @@ function New-BcImage {
                     $isolation = "process"
                 }
             }
+            elseif ("$hostOsVersion".StartsWith('10.0.19043.') -and "$containerOsVersion".StartsWith("10.0.19041.")) {
+                if ($isolation -eq "") {
+                    Write-Host -ForegroundColor Yellow "WARNING: Host OS is 21H1 and Container OS is 2004, defaulting to process isolation. If you experience problems, add -isolation hyperv."
+                    $isolation = "process"
+                }
+            }
             else {
                 if ($isolation -eq "") {
                     if ($isAdministrator) {
