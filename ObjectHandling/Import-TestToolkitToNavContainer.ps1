6e54d246ff23622a6d499f006cd023dad06089ea
Freddy Kristiansen <freddyk@microsoft.com>
2021-10-15 13:32:58 +0200

    support both name styles

--- ObjectHandling/Import-TestToolkitToNavContainer.ps1
+++ ObjectHandling/Import-TestToolkitToNavContainer.ps1
@@ -148,7 +148,7 @@ try {
             $appFiles = GetTestToolkitApps -containerName $containerName -includeTestRunnerOnly:$includeTestRunnerOnly -includeTestFrameworkOnly:$includeTestFrameworkOnly -includeTestLibrariesOnly:$includeTestLibrariesOnly -includePerformanceToolkit:$includePerformanceToolkit
 
             $publishParams = @{}
-            if ($version.Major -ge 18 -and $version.Major -lt 20 -and ($appFiles | Where-Object { $_.Name -eq "Microsoft_Performance Toolkit.app" })) {
+            if ($version.Major -ge 18 -and $version.Major -lt 20 -and ($appFiles | Where-Object { $_.Name -eq "Microsoft_Performance Toolkit.app" -or $_.Name -like "Microsoft_Performance Toolkit_*.*.*.*.app" })) {
                 $BCPTLogEntryAPIsrc = Join-Path $PSScriptRoot "..\AppHandling\BCPTLogEntryAPI"
                 $appJson = Get-Content -path (Join-Path $BCPTLogEntryAPIsrc "app.json") | ConvertFrom-Json
                 $internalsVisibleTo = @{ "id" = $appJson.id; "name" = $appJson.name; "publisher" = $appjson.publisher }
