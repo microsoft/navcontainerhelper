a65a6ff81cf2dbc6848b0e00d6d8cdb163e0af74
Freddy Kristiansen <freddyk@microsoft.com>
2022-03-14 06:12:32 +0100

    dump

--- AppHandling/Run-AlPipeline.ps1
+++ AppHandling/Run-AlPipeline.ps1
@@ -844,10 +844,27 @@ Measure-Command {
 } | ForEach-Object { Write-Host -ForegroundColor Yellow "`nCreating container took $([int]$_.TotalSeconds) seconds" }
 if ($gitHubActions) { Write-Host "::endgroup::" }
 
-Write-Host "Resolving dependencies"
+if ($gitHubActions) { Write-Host "::group::Installing apps" }
+Write-Host -ForegroundColor Yellow @'
+
+ _____                _       _                   _                           _                 _          
+|  __ \              | |     (_)                 | |                         | |               (_)         
+| |__) |___ ___  ___ | |_   ___ _ __   __ _    __| | ___ _ __   ___ _ __   __| | ___ _ __   ___ _  ___ ___ 
+|  _  // _ \ __|/ _ \| \ \ / / | '_ \ / _` |  / _` |/ _ \ '_ \ / _ \ '_ \ / _` |/ _ \ '_ \ / __| |/ _ \ __|
+| | \ \  __\__ \ (_) | |\ V /| | | | | (_| | | (_| |  __/ |_) |  __/ | | | (_| |  __/ | | | (__| |  __\__ \
+|_|  \_\___|___/\___/|_| \_/ |_|_| |_|\__, |  \__,_|\___| .__/ \___|_| |_|\__,_|\___|_| |_|\___|_|\___|___/
+                                       __/ |            | |                                                
+                                      |___/             |_|                                                
+
+'@
 $unknownDependencies = @()
 $sortedFolders = @(Sort-AppFoldersByDependencies -appFolders ($appFolders+$testFolders+$bcptTestFolders) -WarningAction SilentlyContinue -unknownDependencies ([ref]$unknownDependencies))
+Write-Host "Sorted folders"
+$sortedFolders | ForEach-Object { Write-Host "- $_" }
+Write-Host "External dependencies"
+$unknownDependencies | ForEach-Object { Write-Host "- $_" }
 $unknownDependencies = $unknownDependencies | ForEach-Object { $_.Split(':')[0] }
+if ($gitHubActions) { Write-Host "::endgroup::" }
 
 if ($installApps) {
 if ($gitHubActions) { Write-Host "::group::Installing apps" }
