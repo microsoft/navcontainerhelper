c52776f45b345df95882507985b582177d152726
freddyk <freddyk@microsoft.com>
2022-05-19 13:17:32 +0200

    dump

--- ContainerHandling/Invoke-ScriptInNavContainer.ps1
+++ ContainerHandling/Invoke-ScriptInNavContainer.ps1
@@ -222,10 +222,11 @@ if ($exception) {
 
 " [System.Management.Automation.PSSerializer]::Serialize(`$result) | Set-Content '$outputFile' " | Add-Content $file
 
-#Write-Host -ForegroundColor cyan (Get-Content $file -Raw -Encoding UTF8)
+Write-Host -ForegroundColor cyan (Get-Content $file -Raw -Encoding UTF8)
 
                 $ErrorActionPreference = "Stop"
                 docker exec $containerName powershell $file | Out-Host
+Write-Host "docker done"
                 if($LASTEXITCODE -ne 0) {
                     Remove-Item $file -Force -ErrorAction SilentlyContinue
                     Remove-Item $outputFile -Force -ErrorAction SilentlyContinue
@@ -233,9 +234,11 @@ if ($exception) {
                 }
                 if (Test-Path -Path $outputFile -PathType Leaf) {
                     $result = [System.Management.Automation.PSSerializer]::Deserialize((Get-content $outputFile))
+$result | Out-Host
                     $exception = $result | Where-Object { $_ -like "::EXCEPTION::*" }
                     if ($exception) {
                         $errorMessage = $exception.SubString(13)
+Write-Host "ERROR IS $errorMessage"
                         throw $errorMessage
                     }
                     $result
