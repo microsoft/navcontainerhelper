566d5608771217db153639ca656b1c9b77afcfc6
Daniel Göhler <d.goehler@prisma-informatik.de>
2022-05-20 20:07:58 +0200

    Set-BcContainerServerConfiguration and Restart-BcContainerServiceTier added

---
+++ ContainerHandling/Restart-BcContainerServiceTier.ps1
@@ -0,0 +1,36 @@
+﻿<# 
+ .Synopsis
+  Restarts a Business Central Server instance inside of an Business Central Container.
+ .Description
+  The Restart-BcContainerServiceTier cmldet stops a server instance, and then starts it again.
+  You will typically use the Restart-BcContainerServiceTier cmdlet after you make changes to the 
+  server instance configuration using the Set-BcContainerServerConfiguration cmdlet, because most 
+  configuration changes will not take effect until the server instance is restarted.
+
+  Be aware that when you restart the server instance, all client connections to the server instance are terminated.
+ .Parameter containerName
+  Name of container which Business Central Server you want to restart
+ .Example
+  Restart-BcContainerServiceTier -containerName "MyContainer"
+#>
+function Restart-BcContainerServiceTier {
+    Param (
+        [String] $ContainerName = $bcContainerHelperConfig.defaultContainerName
+    )
+
+$telemetryScope = InitTelemetryScope -name $MyInvocation.InvocationName -parameterValues $PSBoundParameters -includeParameters @()
+try {
+    $ResultObjectArray = @()
+    $config = Invoke-ScriptInBcContainer -containerName $containerName -ScriptBlock{
+        Get-NavServerInstance | Restart-NAVServerInstance
+    }
+}
+catch {
+    TrackException -telemetryScope $telemetryScope -errorRecord $_
+    throw
+}
+finally {
+    TrackTrace -telemetryScope $telemetryScope
+}
+}
+Export-ModuleMember -Function Restart-BcContainerServiceTier
