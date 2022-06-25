44355ddd30ee53c681fb0e403df896dea2b8361e
freddydk <freddyk@microsoft.com>
2022-06-25 11:57:12 +0200

    split tests

---
+++ Tests/GetAndRunTests (14).Tests.ps1
@@ -0,0 +1,56 @@
+﻿Param(
+    [string] $licenseFile,
+    [string] $buildlicenseFile,
+    [string] $insiderSasToken
+)
+
+BeforeAll {
+    . (Join-Path $PSScriptRoot '_TestHelperFunctions.ps1')
+    $appPublisher = "Cronus Denmark A/S"
+    $appName = "Hello ÆØÅ"
+    $appVersion = "1.0.0.0"
+    $runTestsInVersion  = 14
+    $navContainerName = "navserver"
+}
+
+AfterAll {
+    . (Join-Path $PSScriptRoot '_RemoveNavContainer.ps1')
+}
+
+Describe 'AppHandling' {
+
+    It "Get/RunTests for version $runTestsInVersion" {
+        $artifactUrl = Get-BCArtifactUrl -type OnPrem -version "$runTestsInVersion" -country "w1" -select Latest
+        New-NavContainer -accept_eula `
+                         -accept_outdated `
+                         -containerName $navContainerName `
+                         -artifactUrl $artifactUrl `
+                         -auth NavUserPassword `
+                         -Credential $credential `
+                         -updateHosts `
+                         -licenseFile $licenseFile `
+                         -includeTestToolkit
+
+        Copy-Item -Path (Join-Path $PSScriptRoot "inserttests") -Destination (Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$navContainerName") -Recurse -Force
+        $appProjectFolder = Join-Path (Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$navContainerName") "inserttests"
+        Compile-AppInNavContainer -containerName $navContainerName -credential $credential -appProjectFolder $appProjectFolder -appOutputFolder $appProjectFolder -appName "inserttests.app" -UpdateSymbols
+        Publish-NavContainerApp -containerName $navContainerName -appFile (Join-Path $appProjectFolder "inserttests.app") -skipVerification -sync -install
+
+        $tests = (Get-TestsFromNavContainer -containerName $navContainerName -credential $credential).Codeunits
+        $tests.Count | Should -be 2
+    
+        $first = $true
+        $resultsFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$navContainerName\result.xml"
+        $tests | % {
+            $allpassed = Run-TestsInNavContainer -containerName $navContainerName `
+                                                 -credential $credential `
+                                                 -XUnitResultFileName $resultsFile `
+                                                 -AppendToXUnitResultFile:(!$first) `
+                                                 -detailed `
+                                                 -testCodeunit $_.Id `
+                                                 -returnTrueIfAllPassed
+            $first = $false
+        }
+        $resultsFile | Should -Exist
+    }
+}
---
+++ Tests/GetAndRunTests (17).Tests.ps1
@@ -0,0 +1,52 @@
+﻿Param(
+    [string] $licenseFile,
+    [string] $buildlicenseFile,
+    [string] $insiderSasToken
+)
+
+BeforeAll {
+    . (Join-Path $PSScriptRoot '_TestHelperFunctions.ps1')
+    $appPublisher = "Cronus Denmark A/S"
+    $appName = "Hello ÆØÅ"
+    $appVersion = "1.0.0.0"
+    $bcAppId = "cb99d78b-f9db-4a1e-822a-0c9c444535df"
+    $runTestsInVersion  = 17
+    $bcContainerName = "bcserver"
+}
+
+AfterAll {
+    . (Join-Path $PSScriptRoot '_RemoveBcContainer.ps1')
+}
+
+Describe 'AppHandling' {
+
+   It "Get/RunTests for version $runTestsInVersion" {
+        $artifactUrl = Get-BCArtifactUrl -type OnPrem -version "$runTestsInVersion" -country "w1" -select Latest
+        New-BcContainer -accept_eula `
+                        -accept_outdated `
+                        -containerName $bcContainerName `
+                        -artifactUrl $artifactUrl `
+                        -auth NavUserPassword `
+                        -Credential $credential `
+                        -updateHosts `
+                        -licenseFile $licenseFile `
+                        -includeTestToolkit
+        
+        $tests = (Get-TestsFromBCContainer -containerName $runTestsContainerName -credential $credential -extensionId "fa3e2564-a39e-417f-9be6-c0dbe3d94069") | Where-Object { $_.id -eq 134006 -or $_.id -eq 134007 }
+        $tests.Count | Should -be 2
+
+        $first = $true
+        $resultsFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$runTestsContainerName\result.xml"
+        $tests | ForEach-Object {
+            Run-TestsInBcContainer -containerName $runTestsContainerName `
+                                   -credential $credential `
+                                   -XUnitResultFileName $resultsFile `
+                                   -AppendToXUnitResultFile:(!$first) `
+                                   -detailed `
+                                   -testCodeunit $_.Id `
+                                   -returnTrueIfAllPassed | Out-Null
+            $first = $false
+        }
+        $resultsFile | Should -Exist
+    }
+}
