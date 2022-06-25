46b319a1615bae29d97582bb57674c371f843d7c
freddydk <freddyk@microsoft.com>
2022-06-25 06:05:45 +0200

    run apphandling nav

--- Tests/_TestRunOne.ps1
+++ Tests/_TestRunOne.ps1
@@ -2,6 +2,7 @@
     [string] $licenseFile = ($nchLicensefileSecret.secretValue | Get-PlainText),
     [string] $buildlicenseFile = ($LicensefileSecret.secretValue | Get-PlainText),
     [string] $insiderSasToken = ($InsiderSasTokenSecret.SecretValue | Get-PlainText)
+    [string] $testScript = 'AppHandling.ps1'
 )
 
 . (Join-Path $PSScriptRoot '_TestHelperFunctions.ps1')
@@ -15,4 +16,4 @@ $credential = [PSCredential]::new("admin", (ConvertTo-SecureString -AsPlainText 
 . (Join-Path $PSScriptRoot '_CreateNavContainer.ps1')
 . (Join-Path $PSScriptRoot '_CreateBcContainer.ps1')
 
-. (Join-Path $PSScriptRoot "AppHandling.ps1")
+. (Join-Path $PSScriptRoot $testScript)
