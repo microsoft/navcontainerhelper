8e7879dee8164b14ea533b478a7cf311931be27d
freddydk <freddyk@microsoft.com>
2022-06-25 10:55:51 +0200

    new syntax

--- Tests/PackageHandling.Tests.ps1
+++ Tests/PackageHandling.Tests.ps1
@@ -13,10 +13,10 @@ AfterAll {
 
 Describe 'PackageHandling' {
     It 'Get-AzureFeedWildcardVersion' {
-        (Get-AzureFeedWildcardVersion -appVersion "1.0.0") | Should Be "1.*.*"
-        (Get-AzureFeedWildcardVersion -appVersion "1.0.0") | Should Not Be "1.0.*"
-        (Get-AzureFeedWildcardVersion -appVersion "1.0.319") | Should Be "1.0.319"
-        (Get-AzureFeedWildcardVersion -appVersion "1.0.319") | Should Not Be "1.*.319"
+        (Get-AzureFeedWildcardVersion -appVersion "1.0.0") | Should -be "1.*.*"
+        (Get-AzureFeedWildcardVersion -appVersion "1.0.0") | Should -not -be "1.0.*"
+        (Get-AzureFeedWildcardVersion -appVersion "1.0.319") | Should -be "1.0.319"
+        (Get-AzureFeedWildcardVersion -appVersion "1.0.319") | Should -not -be "1.*.319"
     }
     It 'Resolve-DependenciesFromAzureFeed' {
         Resolve-DependenciesFromAzureFeed -organization "https://dev.azure.com/TEST/" -feed "BCApps" -appsFolder (Join-Path $PSScriptRoot ".\helloworld")
