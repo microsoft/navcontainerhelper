df1688f0041429923e86365044320fa233a23fc9
freddydk <freddyk@microsoft.com>
2022-12-09 14:44:21 +0100

    casing

--- BC.ArtifactsHelper.psm1
+++ BC.ArtifactsHelper.psm1
@@ -13,5 +13,5 @@ param(
 
 # Common functions
 . (Join-Path $PSScriptRoot "Artifacts\Download-Artifacts.ps1")
-. (Join-Path $PSScriptRoot "Artifacts\Get-BcArtifactUrl.ps1")
+. (Join-Path $PSScriptRoot "Artifacts\Get-BCArtifactUrl.ps1")
 . (Join-Path $PSScriptRoot "Artifacts\Get-NavArtifactUrl.ps1")
