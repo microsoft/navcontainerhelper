5813bb4dfd49d85d943dfe5f23aa2fefaf955e0d
freddydk <freddyk@microsoft.com>
2022-12-09 16:30:37 +0100

    verno

--- BC.HelperFunctions.psm1
+++ BC.HelperFunctions.psm1
@@ -191,20 +191,20 @@ $telemetry = @{
     "TopId" = ""
     "Debug" = $false
 }
-try {
+#try {
     if (($bcContainerHelperConfig.MicrosoftTelemetryConnectionString) -and !$Silent) {
         Write-Host -ForegroundColor Green 'BC.HelperFunctions emits usage statistics telemetry to Microsoft'
     }
-    $dllPath = Join-Path $configHelperFolder 'Microsoft.ApplicationInsights.2.15.0.44797.dll'
+    $dllPath = Join-Path $configHelperFolder 'Microsoft.ApplicationInsights.2.32.0.429.dll'
     if (-not (Test-Path $dllPath)) {
         Copy-Item (Join-Path $PSScriptRoot "Microsoft.ApplicationInsights.dll") -Destination $dllPath
     }
     $telemetry.Assembly = [System.Reflection.Assembly]::LoadFrom($dllPath)
-} catch {
-    if (!$Silent) {
-        Write-Host -ForegroundColor Yellow "Unable to load ApplicationInsights.dll"
-    }
-}
+#} catch {
+#    if (!$Silent) {
+#        Write-Host -ForegroundColor Yellow "Unable to load ApplicationInsights.dll"
+#    }
+#}
 
 . (Join-Path $PSScriptRoot "TelemetryHelper.ps1")
 
