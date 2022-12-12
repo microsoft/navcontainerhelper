9529ec8ede696bc4b5c108cbd3d41b3eb86bbb18
freddydk <freddyk@microsoft.com>
2022-12-12 23:12:19 +0100

    fix order

--- BcContainerHelper.psm1
+++ BcContainerHelper.psm1
@@ -5,7 +5,7 @@ param(
     [switch] $useVolumes
 )
 
-'BC.HelperFunctions','BC.ArtifactsHelper.psm1','BC.AppSourceHelper.psm1','BC.ALGoHelper.psm1','BC.SaasHelper.psm1','BC.NuGetHelper.psm1','BC' | ForEach-Object {
+'BC.NuGetHelper.psm1','BC.SaasHelper.psm1','BC.ALGoHelper.psm1','BC.AppSourceHelper.psm1','BC.ArtifactsHelper.psm1','BC.HelperFunctions' | ForEach-Object {
     if (Get-Module $_) { Remove-Module $_}
 }
 
@@ -122,16 +122,9 @@ Import-Module (Join-Path $PSScriptRoot 'BC.NuGetHelper.psm1') -DisableNameChecki
 . (Join-Path $PSScriptRoot "ContainerInfo\Get-NavContainerImageLabels")
 . (Join-Path $PSScriptRoot "ContainerInfo\Get-NavContainerImageTags")
 
-# Api Functions
-. (Join-Path $PSScriptRoot "Api\Get-NavContainerApiCompanyId.ps1")
-. (Join-Path $PSScriptRoot "Api\Invoke-NavContainerApi.ps1")
-
 # Container Handling Functions
 . (Join-Path $PSScriptRoot "ContainerHandling\Get-NavContainerSession.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Remove-NavContainerSession.ps1")
-. (Join-Path $PSScriptRoot "ContainerHandling\Enter-NavContainer.ps1")
-. (Join-Path $PSScriptRoot "ContainerHandling\Open-NavContainer.ps1")
-. (Join-Path $PSScriptRoot "ContainerHandling\New-NavContainerWizard.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\New-NavContainer.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\New-NavImage.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Restart-NavContainer.ps1")
@@ -146,34 +139,49 @@ Import-Module (Join-Path $PSScriptRoot 'BC.NuGetHelper.psm1') -DisableNameChecki
 . (Join-Path $PSScriptRoot "ContainerHandling\Get-BestNavContainerImageName.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Get-BestGenericImageName.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Invoke-ScriptInNavContainer.ps1")
-. (Join-Path $PSScriptRoot "ContainerHandling\Setup-TraefikContainerForNavContainers.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Flush-ContainerHelperCache.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Get-LatestAlLanguageExtensionUrl.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Get-AlLanguageExtensionFromArtifacts.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\traefik\Add-DomainToTraefikConfig.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Set-BcContainerServerConfiguration.ps1")
 . (Join-Path $PSScriptRoot "ContainerHandling\Restart-BcContainerServiceTier.ps1")
-
-# Object Handling functions
-. (Join-Path $PSScriptRoot "ObjectHandling\Export-NavContainerObjects.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Create-MyOriginalFolder.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Create-MyDeltaFolder.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Convert-Txt2Al.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Export-ModifiedObjectsAsDeltas.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Convert-ModifiedObjectsToAl.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Import-ObjectsToNavContainer.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Import-DeltasToNavContainer.ps1")
 . (Join-Path $PSScriptRoot "ObjectHandling\Import-TestToolkitToNavContainer.ps1")
-. (Join-Path $PSScriptRoot "ObjectHandling\Compile-ObjectsInNavContainer.ps1")
 . (Join-Path $PSScriptRoot "ObjectHandling\Invoke-NavContainerCodeunit.ps1")
 
-# AL-Go for GitHub functions
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoAuthContext.ps1")
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoAppSourceContext.ps1")
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoStorageContext.ps1")
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoNuGetContext.ps1")
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoRepo.ps1")
-#. (Join-Path $PSScriptRoot "AL-Go\New-ALGoRepoWizard.ps1")
+# Tenant Handling functions
+. (Join-Path $PSScriptRoot "TenantHandling\New-NavContainerTenant.ps1")
+. (Join-Path $PSScriptRoot "TenantHandling\Remove-NavContainerTenant.ps1")
+. (Join-Path $PSScriptRoot "TenantHandling\Get-NavContainerTenants.ps1")
+
+# Bacpac Handling functions
+. (Join-Path $PSScriptRoot "Bacpac\Export-NavContainerDatabasesAsBacpac.ps1")
+. (Join-Path $PSScriptRoot "Bacpac\Backup-NavContainerDatabases.ps1")
+. (Join-Path $PSScriptRoot "Bacpac\Restore-DatabasesInNavContainer.ps1")
+. (Join-Path $PSScriptRoot "Bacpac\Restore-BcDatabaseFromArtifacts.ps1")
+. (Join-Path $PSScriptRoot "Bacpac\Remove-BcDatabase.ps1")
+
+# User Handling functions
+. (Join-Path $PSScriptRoot "UserHandling\Get-NavContainerNavUser.ps1")
+. (Join-Path $PSScriptRoot "UserHandling\New-NavContainerNavUser.ps1")
+. (Join-Path $PSScriptRoot "UserHandling\New-NavContainerWindowsUser.ps1")
+. (Join-Path $PSScriptRoot "UserHandling\Setup-NavContainerTestUsers.ps1")
+
+# Misc functions
+. (Join-Path $PSScriptRoot "Misc\Write-NavContainerHelperWelcomeText.ps1")
+. (Join-Path $PSScriptRoot "Misc\Get-LocaleFromCountry.ps1")
+. (Join-Path $PSScriptRoot "Misc\Get-NavVersionFromVersionInfo.ps1")
+. (Join-Path $PSScriptRoot "Misc\Copy-FileFromNavContainer.ps1")
+. (Join-Path $PSScriptRoot "Misc\Copy-FileToNavContainer.ps1")
+. (Join-Path $PSScriptRoot "Misc\Add-FontsToNavContainer.ps1")
+. (Join-Path $PSScriptRoot "Misc\Set-BcContainerFeatureKeys.ps1")
+. (Join-Path $PSScriptRoot "Misc\Import-PfxCertificateToNavContainer.ps1")
+. (Join-Path $PSScriptRoot "Misc\Import-CertificateToNavContainer.ps1")
+
+# Company Handling functions
+. (Join-Path $PSScriptRoot "CompanyHandling\Copy-CompanyInNavContainer.ps1")
+. (Join-Path $PSScriptRoot "CompanyHandling\Get-CompanyInNavContainer.ps1")
+. (Join-Path $PSScriptRoot "CompanyHandling\New-CompanyInNavContainer.ps1")
+. (Join-Path $PSScriptRoot "CompanyHandling\Remove-CompanyInNavContainer.ps1")
 
 # App Handling functions
 . (Join-Path $PSScriptRoot "AppHandling\Publish-NavContainerApp.ps1")
@@ -208,97 +216,42 @@ Import-Module (Join-Path $PSScriptRoot 'BC.NuGetHelper.psm1') -DisableNameChecki
 . (Join-Path $PSScriptRoot "AppHandling\Run-AlValidation.ps1")
 . (Join-Path $PSScriptRoot "AppHandling\Run-AlCops.ps1")
 
-# Tenant Handling functions
-. (Join-Path $PSScriptRoot "TenantHandling\New-NavContainerTenant.ps1")
-. (Join-Path $PSScriptRoot "TenantHandling\Remove-NavContainerTenant.ps1")
-. (Join-Path $PSScriptRoot "TenantHandling\Get-NavContainerTenants.ps1")
-
-# Bacpac Handling functions
-. (Join-Path $PSScriptRoot "Bacpac\Export-NavContainerDatabasesAsBacpac.ps1")
-. (Join-Path $PSScriptRoot "Bacpac\Backup-NavContainerDatabases.ps1")
-. (Join-Path $PSScriptRoot "Bacpac\Restore-DatabasesInNavContainer.ps1")
-. (Join-Path $PSScriptRoot "Bacpac\Restore-BcDatabaseFromArtifacts.ps1")
-. (Join-Path $PSScriptRoot "Bacpac\Remove-BcDatabase.ps1")
-
-# User Handling functions
-. (Join-Path $PSScriptRoot "UserHandling\Get-NavContainerNavUser.ps1")
-. (Join-Path $PSScriptRoot "UserHandling\New-NavContainerNavUser.ps1")
-. (Join-Path $PSScriptRoot "UserHandling\New-NavContainerWindowsUser.ps1")
-. (Join-Path $PSScriptRoot "UserHandling\Setup-NavContainerTestUsers.ps1")
-
-# Azure AD specific functions
-. (Join-Path $PSScriptRoot "AzureAD\Create-AadAppsForNav.ps1")
-. (Join-Path $PSScriptRoot "AzureAD\Create-AadUsersInNavContainer.ps1")
-
-# AppSource specific functions
-#. (Join-Path $PSScriptRoot "AppSource\Invoke-IngestionAPI.ps1")
-#. (Join-Path $PSScriptRoot "AppSource\Get-AppSourceProduct.ps1")
-#. (Join-Path $PSScriptRoot "AppSource\Get-AppSourceSubmission.ps1")
-#. (Join-Path $PSScriptRoot "AppSource\New-AppSourceSubmission.ps1")
-#. (Join-Path $PSScriptRoot "AppSource\Promote-AppSourceSubmission.ps1")
-#. (Join-Path $PSScriptRoot "AppSource\Cancel-AppSourceSubmission.ps1")
-
-# Nuget specific functions
-#. (Join-Path $PSScriptRoot "NuGet\New-BcNuGetPackage.ps1")
-#. (Join-Path $PSScriptRoot "NuGet\Get-BcNuGetPackage.ps1")
-#. (Join-Path $PSScriptRoot "NuGet\Push-BcNuGetPackage.ps1")
-#. (Join-Path $PSScriptRoot "NuGet\Publish-BcNuGetPackageToContainer.ps1")
-
-# BC SaaS specific functions
-#. (Join-Path $PSScriptRoot "SaaS\New-BcAuthContext.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Renew-BcAuthContext.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Get-BcEnvironments.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Get-BcPublishedApps.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Get-BcInstalledExtensions.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Install-BcAppFromAppSource")
-#. (Join-Path $PSScriptRoot "SaaS\Publish-PerTenantExtensionApps.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\New-BcEnvironment.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Remove-BcEnvironment.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Set-BcEnvironmentApplicationInsightsKey.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Get-BcDatabaseExportHistory.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\New-BcDatabaseExport.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Get-BcScheduledUpgrade.ps1")
-#. (Join-Path $PSScriptRoot "SaaS\Reschedule-BcUpgrade.ps1")
-
-# Azure VM specific functions
-. (Join-Path $PSScriptRoot "AzureVM\Replace-NavServerContainer.ps1")
-. (Join-Path $PSScriptRoot "AzureVM\New-LetsEncryptCertificate.ps1")
-. (Join-Path $PSScriptRoot "AzureVM\Renew-LetsEncryptCertificate.ps1")
-
-# Misc functions
-. (Join-Path $PSScriptRoot "Misc\Write-NavContainerHelperWelcomeText.ps1")
-. (Join-Path $PSScriptRoot "Misc\Get-LocaleFromCountry.ps1")
-. (Join-Path $PSScriptRoot "Misc\Get-NavVersionFromVersionInfo.ps1")
-. (Join-Path $PSScriptRoot "Misc\Copy-FileFromNavContainer.ps1")
-. (Join-Path $PSScriptRoot "Misc\Copy-FileToNavContainer.ps1")
-. (Join-Path $PSScriptRoot "Misc\Add-FontsToNavContainer.ps1")
-. (Join-Path $PSScriptRoot "Misc\Set-BcContainerFeatureKeys.ps1")
-. (Join-Path $PSScriptRoot "Misc\Import-PfxCertificateToNavContainer.ps1")
-. (Join-Path $PSScriptRoot "Misc\Import-CertificateToNavContainer.ps1")
-
-#. (Join-Path $PSScriptRoot "Artifacts\Download-Artifacts.ps1")
-#. (Join-Path $PSScriptRoot "Artifacts\Get-BcArtifactUrl.ps1")
-#. (Join-Path $PSScriptRoot "Artifacts\Get-NavArtifactUrl.ps1")
-
-#. (Join-Path $PSScriptRoot "Common\Download-File.ps1")
-#. (Join-Path $PSScriptRoot "Common\New-DesktopShortcut.ps1")
-#. (Join-Path $PSScriptRoot "Common\Remove-DesktopShortcut.ps1")
-#. (Join-Path $PSScriptRoot "Common\ConvertTo-HashTable.ps1")
-#. (Join-Path $PSScriptRoot "Common\Get-PlainText.ps1")
-#. (Join-Path $PSScriptRoot "Common\Invoke-gh.ps1")
-#. (Join-Path $PSScriptRoot "Common\Invoke-git.ps1")
-#. (Join-Path $PSScriptRoot "Common\ConvertTo-OrderedDictionary.ps1")
-
-# Company Handling functions
-. (Join-Path $PSScriptRoot "CompanyHandling\Copy-CompanyInNavContainer.ps1")
-. (Join-Path $PSScriptRoot "CompanyHandling\Get-CompanyInNavContainer.ps1")
-. (Join-Path $PSScriptRoot "CompanyHandling\New-CompanyInNavContainer.ps1")
-. (Join-Path $PSScriptRoot "CompanyHandling\Remove-CompanyInNavContainer.ps1")
+# Api Functions
+. (Join-Path $PSScriptRoot "Api\Get-NavContainerApiCompanyId.ps1")
+. (Join-Path $PSScriptRoot "Api\Invoke-NavContainerApi.ps1")
 
 # Configuration Package Handling
 . (Join-Path $PSScriptRoot "ConfigPackageHandling\Import-ConfigPackageInNavContainer.ps1")
 . (Join-Path $PSScriptRoot "ConfigPackageHandling\Remove-ConfigPackageInNavContainer.ps1")
 . (Join-Path $PSScriptRoot "ConfigPackageHandling\UploadImportAndApply-ConfigPackageInBcContainer.ps1")
+
+# Container Handling functions
+. (Join-Path $PSScriptRoot "ContainerHandling\Enter-NavContainer.ps1")
+. (Join-Path $PSScriptRoot "ContainerHandling\Open-NavContainer.ps1")
+. (Join-Path $PSScriptRoot "ContainerHandling\New-NavContainerWizard.ps1")
+
+# Traefik Handling functions
+. (Join-Path $PSScriptRoot "ContainerHandling\Setup-TraefikContainerForNavContainers.ps1")
+
+# Object Handling functions
+. (Join-Path $PSScriptRoot "ObjectHandling\Export-NavContainerObjects.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Create-MyOriginalFolder.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Create-MyDeltaFolder.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Convert-Txt2Al.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Export-ModifiedObjectsAsDeltas.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Convert-ModifiedObjectsToAl.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Import-ObjectsToNavContainer.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Import-DeltasToNavContainer.ps1")
+. (Join-Path $PSScriptRoot "ObjectHandling\Compile-ObjectsInNavContainer.ps1")
+
+# Azure AD specific functions
+. (Join-Path $PSScriptRoot "AzureAD\Create-AadAppsForNav.ps1")
+. (Join-Path $PSScriptRoot "AzureAD\Create-AadUsersInNavContainer.ps1")
+
+# Azure VM specific functions
+. (Join-Path $PSScriptRoot "AzureVM\Replace-NavServerContainer.ps1")
+. (Join-Path $PSScriptRoot "AzureVM\New-LetsEncryptCertificate.ps1")
+. (Join-Path $PSScriptRoot "AzureVM\Renew-LetsEncryptCertificate.ps1")
 
 # Symbol Handling
 . (Join-Path $PSScriptRoot "SymbolHandling\Generate-SymbolsInNavContainer.ps1")
