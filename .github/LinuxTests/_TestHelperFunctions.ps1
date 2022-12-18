38129e538ff02a0fbb70966ebc1a339e7979a340
freddydk <freddyk@microsoft.com>
2022-12-18 07:40:53 +0100

    add linux test

---
+++ .github/LinuxTests/_TestHelperFunctions.ps1
@@ -0,0 +1,7 @@
+﻿$modulePath = Join-Path $PSScriptRoot "..\BcContainerHelper.psd1"
+Remove-Module BcContainerHelper -ErrorAction Ignore
+Import-Module $modulePath -DisableNameChecking
+
+$modulePath = Join-Path $PSScriptRoot "..\BcContainerHelper.psm1"
+Remove-Module BcContainerHelper -ErrorAction Ignore
+Import-Module $modulePath -DisableNameChecking
