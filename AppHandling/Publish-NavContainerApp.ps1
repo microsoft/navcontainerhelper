d731ac1d580696e458e42a5f962092d04f06cf30
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2023-05-25 05:42:48 +0200

    remove sleep

--- AppHandling/Publish-NavContainerApp.ps1
+++ AppHandling/Publish-NavContainerApp.ps1
@@ -299,9 +299,6 @@ try {
             }
             else {
                 Invoke-ScriptInBcContainer -containerName $containerName -ScriptBlock { Param($appFile, $skipVerification, $sync, $install, $upgrade, $tenant, $syncMode, $packageType, $scope, $language, $PublisherAzureActiveDirectoryTenantId, $force, $ignoreIfAppExists)
-                    Write-Host "sleeping for 60 seconds"
-                    Start-Sleep -Seconds 60
-                    Write-Host "done sleeping"
                     $publishArgs = @{ "packageType" = $packageType }
                     if ($scope) {
                         $publishArgs += @{ "Scope" = $scope }
