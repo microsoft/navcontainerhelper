32c1f548bba7bd568034a2046f348d92562b1543
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2023-08-24 07:14:09 +0200

    ConvertTo-HashTable to support arrays (#3165)
    Co-authored-by: freddydk <freddydk@users.noreply.github.com>

--- Common/ConvertTo-HashTable.ps1
+++ Common/ConvertTo-HashTable.ps1
@@ -13,25 +13,45 @@ function ConvertTo-HashTable() {
         $object,
         [switch] $recurse
     )
+
+    function AddValueToHashTable {
+        Param(
+            [hashtable] $ht,
+            [string] $name,
+            $value,
+            [switch] $recurse
+        )
+
+        if ($ht.Contains($name)) {
+            throw "Duplicate key $name"
+        }
+        if ($recurse -and ($value -is [System.Collections.Specialized.OrderedDictionary] -or $value -is [hashtable] -or $value -is [System.Management.Automation.PSCustomObject])) {
+            $ht[$name] = ConvertTo-HashTable $value -recurse
+        }
+        elseif ($recurse -and $value -is [array]) {
+            $ht[$name] = @($value | ForEach-Object {
+                if (($_ -is [System.Collections.Specialized.OrderedDictionary]) -or ($_ -is [hashtable]) -or ($_ -is [System.Management.Automation.PSCustomObject])) {
+                    ConvertTo-HashTable $_ -recurse
+                }
+                else {
+                    $_
+                }
+            })
+        }
+        else {
+            $ht[$name] = $value
+        }
+    }
+
     $ht = @{}
     if ($object -is [System.Collections.Specialized.OrderedDictionary] -or $object -is [hashtable]) {
         $object.Keys | ForEach-Object {
-            if ($recurse -and ($object."$_" -is [System.Collections.Specialized.OrderedDictionary] -or $object."$_" -is [hashtable] -or $object."$_" -is [PSCustomObject])) {
-                $ht[$_] = ConvertTo-HashTable $object."$_" -recurse
-            }
-            else {
-                $ht[$_] = $object."$_"
-            }
+            AddValueToHashTable -ht $ht -name $_ -value $object."$_" -recurse:$recurse
         }
     }
-    elseif ($object -is [PSCustomObject]) {
+    elseif ($object -is [System.Management.Automation.PSCustomObject]) {
         $object.PSObject.Properties | ForEach-Object {
-            if ($recurse -and ($_.Value -is [System.Collections.Specialized.OrderedDictionary] -or $_.Value -is [hashtable] -or $_.Value -is [PSCustomObject])) {
-                $ht[$_.Name] = ConvertTo-HashTable $_.Value -recurse
-            }
-            else {
-                $ht[$_.Name] = $_.Value
-            }
+            AddValueToHashTable -ht $ht -name $_.Name -value $_.Value -recurse:$recurse
         }
     }
     $ht
