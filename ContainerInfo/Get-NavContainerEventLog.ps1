118c0ff2c6878e11688b6efa27a4cf2b065f0a4a
Daniel <github@dannoe.de>
2023-11-27 14:14:39 +0100

    Use Start-Process for Get-BcContainerEventLog
    The previous call "[Diagnostics.Process]::Start($eventLogName)" 
    could not open evtx files correctly inside PowerShell 7.
    
    Fixes #3248

--- ContainerInfo/Get-NavContainerEventLog.ps1
+++ ContainerInfo/Get-NavContainerEventLog.ps1
@@ -43,7 +43,7 @@ try {
         $eventLogName
     }
     else {
-        [Diagnostics.Process]::Start($eventLogName) | Out-Null
+        Start-Process -FilePath $eventLogName | Out-Null
     }
 }
 catch {
