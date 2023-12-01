cfdf285b342e5746c4e92f4c2eee3b7759c57625
freddydk <freddydk@users.noreply.github.com>
2023-12-01 06:21:14 +0100

    quotes

--- HelperFunctions.ps1
+++ HelperFunctions.ps1
@@ -1146,8 +1146,7 @@ function GetAppInfo {
             }
             else {
                 if ($alToolExists) {
-                    Write-Host "Using ALTool"
-                    $manifest = CmdDo -Command $alToolExe -arguments @('GetPackageManifest', "$path") -returnValue | ConvertFrom-Json
+                    $manifest = CmdDo -Command $alToolExe -arguments @('GetPackageManifest', """$path""") -returnValue | ConvertFrom-Json
                     $appInfo = @{
                         "appId"                 = $manifest.id
                         "publisher"             = $manifest.publisher
@@ -1160,7 +1159,6 @@ function GetAppInfo {
                     }
                 }
                 else {
-                    Write-Host "Using CodeAnalysis DLL"
                     if (!$assembliesAdded) {
                         Add-Type -AssemblyName System.IO.Compression.FileSystem
                         Add-Type -AssemblyName System.Text.Encoding
