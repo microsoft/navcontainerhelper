0094fd2ff6919ba75026ce97509b2389439ba377
freddydk <freddydk@users.noreply.github.com>
2024-03-30 06:12:29 +0100

    issue #3454

--- ContainerHandling/Get-BestGenericImageName.ps1
+++ ContainerHandling/Get-BestGenericImageName.ps1
@@ -51,57 +51,8 @@ try {
     else {
         $genericImageNameSetting = $bcContainerHelperConfig.genericImageName.Replace('{1}', $ltscVersion)
     }
-    $repo = $genericImageNameSetting.Split(':')[0]
-    $tag = $genericImageNameSetting.Split(':')[1].Replace('{0}','*')
-    if ($tag.indexOf('*') -lt 0) {
-        $genericImageNameSetting
-    }
-    else {
-        $failureDelay = 2
-        while ($true) {
-            $imagetags = Get-BcContainerImageTags -imageName $repo
-            if ($imagetags) {
-                $ver = [Version]"0.0.0.0"
-                # $tag can be *-filesonly, *-filesonly-dev, *-dev or other patterns
-                # * is the Windows version OS version
-                $versions = $imagetags.tags |
-                                Where-Object { $_ -like $tag -and [System.Version]::TryParse($_.SubString($tag.indexOf('*'), $_.length-$tag.length+1), [ref]$ver) } |
-                                ForEach-Object { [System.Version]($_.SubString($tag.indexOf('*'), $_.length-$tag.length+1)) }
-                break
-            }
-            else {
-                if ($failureDelay -gt 32) {
-                    throw "Unable to download image tags for $repo"
-                }
-                Write-Host -ForegroundColor Yellow "Unable to download image tags for $repo, retrying in $failureDelay seconds"
-                Start-Sleep -Seconds $failureDelay
-                $failureDelay = $failureDelay * 2
-            }
-        }
-        
-        $genericImageName = ""
-        $myversions = $versions | Where-Object { $_.Major -eq $hostOsVersion.Major -and $_.Minor -eq $hostOsVersion.Minor -and $_.Build -eq $hostOsVersion.Build } | Sort-Object
-        if (-not $myversions) {
-            if (-not $onlyMatchingBuilds) {
-                if ($hostOsVersion.Build -eq 19043 -or $hostOsVersion.Build -eq 19044 -or $hostOsVersion.Build -eq 19045) {
-                    # 21H1 doesn't work well with 20H2 servercore images - grab 2004 if no corresponding image exists
-                    Write-Host -ForegroundColor Yellow "INFO: Windows 10 21H1/21H2 images are not yet available, using 2004 as these are found to work better than 20H2 on 21H1/21H2"
-                    $myversions = $versions | Where-Object { $_.Build -eq 19041 } | Sort-Object
-                }
-                else {
-                    $myversions = $versions | Sort-Object
-                }
-            }
-        }
-        if ($myversions) {
-            $version = $myversions | Where-Object { $_ -le $hostOsVersion } | Select-Object -Last 1
-            if (-not $version) {
-                $version = $myversions | Select-Object -First 1
-            }
-            $genericImageName = [string]::format($genericImageNameSetting, $version.ToString())
-        }
-        $genericImageName
-    }
+
+    return $genericImageNameSetting.Replace('{0}', $ltscVersion).Replace('{1}', $ltscVersion)
 }
 catch {
     TrackException -telemetryScope $telemetryScope -errorRecord $_
