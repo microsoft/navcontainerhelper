26ccc355a4734b3ac2afb417aa7cfb05b89cc4d0
freddydk <freddydk@users.noreply.github.com>
2024-04-20 14:45:27 +0200

    add config

--- BC.HelperFunctions.ps1
+++ BC.HelperFunctions.ps1
@@ -15,8 +15,9 @@ function Get-ContainerHelperConfig {
             "genericImageNameFilesOnly" = 'mcr.microsoft.com/businesscentral:{1}-filesonly'
             "usePsSession" = $true
             "usePwshForBc24" = $true
-            "useSslForWinRm" = $true
+            "useSslForWinRmSession" = $false
             "tryWinRmSession" = $isPsCore -or !$isAdministrator
+            "alwaysUseWinRmSession" = $false
             "addTryCatchToScriptBlock" = $true
             "killPsSessionProcess" = $false
             "useVolumes" = $false
