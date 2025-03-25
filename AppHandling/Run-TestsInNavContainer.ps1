6070f9ef44097f6265ea40a55f50c528d3e72598
freddydk <freddydk@users.noreply.github.com>
2025-03-25 11:33:25 -0700

    not default container

--- AppHandling/Run-TestsInNavContainer.ps1
+++ AppHandling/Run-TestsInNavContainer.ps1
@@ -86,7 +86,7 @@
 #>
 function Run-TestsInBcContainer {
     Param (
-        [string] $containerName = $bcContainerHelperConfig.defaultContainerName,
+        [string] $containerName = '',
         [string] $compilerFolder = '',
         [Parameter(Mandatory=$false)]
         [string] $tenant = "default",
@@ -143,14 +143,7 @@ function Run-TestsInBcContainer {
 $telemetryScope = InitTelemetryScope -name $MyInvocation.InvocationName -parameterValues $PSBoundParameters -includeParameters @()
 try {
 
-    if ($containerName) {
-        Write-Host "Using Container"
-        $customConfig = Get-BcContainerServerConfiguration -ContainerName $containerName
-        $navversion = Get-BcContainerNavversion -containerOrImageName $containerName
-        $version = [System.Version]($navversion.split('-')[0])
-        $PsTestToolFolder = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$containerName\PsTestTool"
-    }
-    elseif ($compilerFolder) {
+    if ($compilerFolder) {
         Write-Host "Using CompilerFolder"
         $customConfig = $null
         $symbolsFolder = Join-Path $compilerFolder "symbols"
@@ -164,7 +157,14 @@ try {
         Copy-Item -Path (Join-Path $PSScriptRoot "ClientContext.ps1") -Destination $PsTestToolFolder -Force
     }
     else {
-        throw "You must specify either containerName or compilerFolder"
+        if (-not $containerName) {
+            $containerName = $bcContainerHelperConfig.defaultContainerName
+        }
+        Write-Host "Using Container"
+        $customConfig = Get-BcContainerServerConfiguration -ContainerName $containerName
+        $navversion = Get-BcContainerNavversion -containerOrImageName $containerName
+        $version = [System.Version]($navversion.split('-')[0])
+        $PsTestToolFolder = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$containerName\PsTestTool"
     }
 
     if ($bcAuthContext -and $environment) {
