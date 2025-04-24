3feded0f0ad6e2971df10edc60b1120f79cc2ac6
Patrick Schiefer <35697862+PatrickSchiefer@users.noreply.github.com>
2025-04-24 21:50:51 +0200

    Add new option to prevent delete of folder

--- AppHandling/Run-AlPipeline.ps1
+++ AppHandling/Run-AlPipeline.ps1
@@ -875,9 +875,12 @@ if ($updateLaunchJson) {
 
 if ($useCompilerFolder -or $filesOnly -or !$useDevEndpoint) {
     $packagesFolder = CheckRelativePath -baseFolder $baseFolder -sharedFolder $sharedFolder -path $packagesFolder -name "packagesFolder"
-    if (!(Test-Path $packagesFolder)) {
-        New-Item $packagesFolder -ItemType Directory | Out-Null
+    if (!($bcContainerHelperConfig.doNotRemovePackagesFolderIfExists)) {
+        if (Test-Path $packagesFolder) {
+            Remove-Item $packagesFolder -Recurse -Force
+        }
     }
+    New-Item $packagesFolder -ItemType Directory -Force | Out-Null 
 }
 
 if ($useDevEndpoint) {
