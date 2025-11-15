98396683dd6dd1e8b322099596089a70c65a59c2
Freddy Kristiansen <fkr@freddy.dk>
2025-11-15 20:06:46 +0100

    fix tests

--- Tests/NuGet.Tests.ps1
+++ Tests/NuGet.Tests.ps1
@@ -17,12 +17,10 @@ Describe 'Download' {
         @{ "url" = "https://pkgs.dev.azure.com/continia-repository/ContiniaBCPublicFeeds/_packaging/AppSourceApps/nuget/v3/index.json" }
         )
 
-        Measure-Command {
-            $folder = Join-Path ([System.IO.Path]::GetTempPath()) 'nuget'
-            Download-BcNuGetPackageToFolder -packageName "6da8dd2f-e698-461f-9147-8e404244dd85" -version "26.0.0.0" -select LatestMatching -installedApps @(@{"id"="";"name"="Application";"version"="26.5.38752.40172";"publisher"="Microsoft"}) -folder $folder
-        }
+        $folder = Join-Path ([System.IO.Path]::GetTempPath()) 'nuget'
+        Download-BcNuGetPackageToFolder -packageName "6da8dd2f-e698-461f-9147-8e404244dd85" -version "26.0.0.0" -select EarliestMatching -installedApps @(@{"id"="";"name"="Application";"version"="26.5.38752.40172";"publisher"="Microsoft"}) -folder $folder
 
         $files = @(Get-ChildItem $folder)
-        $files.Count | Should -BeGreaterThan 5
+        $files.Count | Should -Be 7
     }
 }
