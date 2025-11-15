4604a7c1bf689a03d176e7244c546c80e7928c65
Freddy Kristiansen <fkr@freddy.dk>
2025-11-15 06:35:23 +0100

    add tests

---
+++ LinuxTests/NuGet.Tests.ps1
@@ -0,0 +1,28 @@
+﻿Param(
+    [string] $licenseFile,
+    [string] $buildlicenseFile
+)
+
+BeforeAll {
+    . (Join-Path $PSScriptRoot '_TestHelperFunctions.ps1')
+}
+
+AfterAll {
+    
+}
+
+Describe 'Download' {
+    It 'Download-BcNuGetPackageToFolder' {
+        $bcContainerHelperConfig.TrustedNuGetFeeds = @(
+        @{ "url" = "https://pkgs.dev.azure.com/continia-repository/ContiniaBCPublicFeeds/_packaging/AppSourceApps/nuget/v3/index.json" }
+        )
+
+        Measure-Command {
+            $folder = Join-Path ([System.IO.Path]::GetTempPath()) 'nuget'
+            Download-BcNuGetPackageToFolder -packageName "6da8dd2f-e698-461f-9147-8e404244dd85" -version "26.0.0.0" -select LatestMatching -installedApps @(@{"id"="";"name"="Application";"version"="26.5.38752.40172";"publisher"="Microsoft"}) -folder $folder
+        }
+
+        $files = @(Get-ChildItem $folder)
+        $files.Count | Should -BeGreaterThan 5
+    }
+}
