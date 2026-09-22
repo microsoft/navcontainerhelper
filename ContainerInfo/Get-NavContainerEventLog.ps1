7b251ffd9fb254ef1fb9b162f67c462a1ddf629c
Freddy Kristiansen <frdk.frd@comp-host.com>
2026-09-22 09:15:59 +0200

    During AL-Go pipelines on Linux, it will try to get the Container Eventlog and fail

--- ContainerInfo/Get-NavContainerEventLog.ps1
+++ ContainerInfo/Get-NavContainerEventLog.ps1
@@ -23,6 +23,10 @@ function Get-BcContainerEventLog {
         [switch] $doNotOpen
     )
 
+    if (!(Test-BcContainer -containerName $containerName)) {
+        return
+    }
+    
     Write-Host "Getting event log for $containername"
 
     $eventLogFolder = Join-Path $bcContainerHelperConfig.hostHelperFolder "EventLogs"
